70d701e67d104b3d4506dc4e5183e5189a9de96a
Tyler K <tyly04@gmail.com>
2023-12-06 14:00:08 -0800

    Prototype bare bones

--- Assets/IlluminateTrigger.cs
+++ Assets/IlluminateTrigger.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 public class IlluminateTrigger : MonoBehaviour
 {
     Light pointLight;
+    Material lanternMaterial;
+    Color pastEmission;
 
     private void Start()
     {
         pointLight = GetComponentInChildren<Light>();
+        var renderer = GetComponentInParent<MeshRenderer>();
+        lanternMaterial = renderer.material;
+        pastEmission = lanternMaterial.GetColor("_EmissionColor");
+        lanternMaterial.SetColor("_EmissionColor", Color.black);
     }
     private void OnTriggerEnter(Collider other)
     {
         pointLight.enabled = true;
+        lanternMaterial.SetColor("_EmissionColor", pastEmission);
     }
 }
